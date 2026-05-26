37c2bce122c6c2ef208b1076bfbf6f45efe5c55a
Oberhauser Péter Zoltán <lifesoffers@gmail.com>
2026-05-26 18:31:55 +0200

    fix: SportNav import typo

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import fs from "fs";
 import path from "path";
 
 import Hero from "./components/Hero";
-import SportsNav from "./components/SportNav";
+import SportNav from "./components/SportNav";
 import MatchCard from "./components/MatchCard";
 import TopBettingSites from "./components/TopBettingSites";
 import Footer from "./components/Footer";
