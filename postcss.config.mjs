c7bda99e702ef0a83e53e7bd3cf396ecfeeb0d46
Oberhauser Péter Zoltán <lifesoffers@gmail.com>
2026-05-27 18:41:30 +0200

    add tailwind config fix UI layout

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,10 @@
-const config = {
-  plugins: {
-    "@tailwindcss/postcss": {},
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
   },
+  plugins: [],
 };
-
-export default config;
