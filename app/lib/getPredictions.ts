0d0278ecfb6e6a329b59de075dbdfc2bf44771b8
Oberhauser Péter Zoltán <lifesoffers@gmail.com>
2026-06-03 11:19:34 +0200

    update

--- app/lib/getPredictions.ts
+++ app/lib/getPredictions.ts
@@ -1,37 +1,5 @@
-import "server-only";
 import predictions from "@/data/predictions.json";
-import type { MarketType } from "@/app/types/match";
 
 export async function getPredictions() {
-  return predictions as {
-    date: string;
-    generatedAt: string;
-    sports: {
-      sport: string;
-      hasMatches: boolean;
-      message?: string;
-      topPicks: {
-        id: string;
-        league: string;
-        eventId: string;
-        homeTeam: string;
-        awayTeam: string;
-        startTime: string;
-        recommendedBet: string;
-        betCode: any;
-        marketType: MarketType; // FIX
-        selectionKey: string;
-        explanation: string;
-        confidence: number;
-        risk: number;
-        odds: number;
-        oddsLabel: string;
-        bookmaker: string;
-        bookmakerUrl: string;
-        ctaLabel: string;
-        isTopPick: boolean;
-        status: "scheduled" | "live" | "finished";
-      }[];
-    }[];
-  };
+  return predictions;
 }
