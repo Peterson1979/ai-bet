d152081c18706a5f5e23ec606e4921974028909c
Oberhauser Péter Zoltán <lifesoffers@gmail.com>
2026-06-03 09:02:50 +0200

    update

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -21,7 +21,7 @@ export default function Hero() {
       </div>
 
       {/* CONTENT */}
-      <div className="absolute top-0 left-0 right-0 z-10 flex flex-col px-2 md:px-6 pt-5 md:pt-8 text-center">
+      <div className="absolute top-0 left-0 right-0 z-10 flex flex-col px-0 md:px-3 pt-5 md:pt-8 text-center">
 
         <h1 className="text-4xl md:text-6xl font-black text-white drop-shadow-md">
           AI Tips
