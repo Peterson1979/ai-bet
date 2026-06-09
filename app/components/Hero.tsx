399bc697c1a66032255afb504fb2e2fefe1ab109
Oberhauser Péter Zoltán <lifesoffers@gmail.com>
2026-06-09 13:34:27 +0200

    feat: favicon and OG image set to logo

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -28,7 +28,7 @@ export default function Hero({ lang = "en" }: Props) {
           <img
             src="/title.png"
             alt="MatchSignal"
-            style={{ height: "220px" }}
+            style={{ height: "440px" }}
             className="object-contain -ml-8"
           />
         </div>
