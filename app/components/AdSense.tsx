35433b48ce3cf5842c00215df652a0ab19f40372
Oberhauser Péter Zoltán <lifesoffers@gmail.com>
2026-07-27 15:15:10 +0200

    Update for AdSense

--- app/components/AdSense.tsx
+++ app/components/AdSense.tsx
@@ -1,12 +1,11 @@
-"use client";
-
 import Script from "next/script";
 
 export default function AdSense() {
   return (
     <Script
       id="adsense-script"
-      strategy="afterInteractive"
+      strategy="beforeInteractive"
+      async
       src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7714565858088235"
       crossOrigin="anonymous"
     />
